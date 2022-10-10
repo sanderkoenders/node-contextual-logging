28dbeecd9c3eed06c91741867f89c62e4c008f32
Sander Koenders <sanderkoenders@gmail.com>
2022-10-10 17:52:37 +0200

    Simplify loggingContext

--- src/lib/loggingContext.ts
+++ src/lib/loggingContext.ts
@@ -14,23 +14,6 @@ export interface IContext {
 const LoggingContext = (transport: Pick<Console, 'log'>) => {
   const asyncLocalStorage = new AsyncLocalStorage<IContext>();
 
-  const log = (level: string, logObj: ILogObj) => {
-    const timeStamp = new Date().toISOString();
-
-    transport.log(
-      JSON.stringify(
-        {
-          timeStamp,
-          level,
-          ...asyncLocalStorage.getStore(),
-          ...logObj,
-        },
-        null,
-        1
-      )
-    );
-  };
-
   return {
     init: (context: IContext, callback: () => void) => {
       asyncLocalStorage.run(context, () => {
@@ -38,15 +21,21 @@ const LoggingContext = (transport: Pick<Console, 'log'>) => {
       });
     },
 
-    debug: (logObj: ILogObj) => log('DEBUG', logObj),
+    log: (logObj: ILogObj) => {
+      const timeStamp = new Date().toISOString();
 
-    log: (logObj: ILogObj) => log('LOG', logObj),
-
-    info: (logObj: ILogObj) => log('INFO', logObj),
-
-    warn: (logObj: ILogObj) => log('WARN', logObj),
-
-    error: (logObj: ILogObj) => log('ERROR', logObj),
+      transport.log(
+        JSON.stringify(
+          {
+            timeStamp,
+            ...asyncLocalStorage.getStore(),
+            ...logObj,
+          },
+          null,
+          1
+        )
+      );
+    },
   };
 };
 
